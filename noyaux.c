ad3c7c6f3b5fdf3c6f353c101aee91494ab2479a
Marie COMPAIN <mariecompain@MacBook-Pro-de-Marie-2.local>
2023-11-10 15:50:33 +0100

    added k in noyaux.c

---
+++ noyaux.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include methodesnum.h
+#include donnees.h
---
+++ noyaux.h
@@ -0,0 +1,6 @@
+#ifndef noyaux_h
+#define noyaux_h
+
+double k(double x, double t);
+
+#endif noyaux_h
