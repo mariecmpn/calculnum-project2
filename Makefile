26e0d307d691bff13dd6c6922d55748b1632a2d7
Lucas Fourny <lucas28@Ubuntu22.myguest.virtualbox.org>
2023-11-15 16:22:37 +0100

    pas grand chose

--- Makefile
+++ Makefile
@@ -3,6 +3,7 @@
 CC = gcc-11
 
 # Regles pour la partie 1 du projet
+
 partie1: main.o methodesnum.o donnees.o fonctions.o
 	$(CC) -o partie1 main.o fonctions.o methodesnum.o donnees.o
 
@@ -19,6 +20,7 @@ main.o: main.c fonctions.c donnees.h methodesnum.h fonctions.h
 	$(CC) -c main.c
 
 # Regles pour la partie 2 du projet
+
 partie2: main2.o methodesnum.o donnees.o fonctions.o fonctions2.o
 	$(CC) -o partie2 main2.o fonctions2.o fonctions.o methodesnum.o donnees.o
 
--- adomain.c
+++ adomain.c
@@ -7,6 +7,16 @@
 #include "donnees.h"
 
 double alpha = 1.;
+double L = recup_L(L);
+
+/* Discrétisation de [0,L]*/
+
+int D = 10; /* Nombre de points sur la discrétisation de [0,L] */
+double dx = L/D;
+
+double *xl = malloc(sizeof(int[D]));
+
+
 
 double u0(double x){
 	double res;
@@ -14,6 +24,19 @@ double u0(double x){
 	return res;
 }
 
+/* Création du tableau U1 qui contient l'évaluation de u_{n+1} en x_l */
+
+double *U1 = malloc(sizeof(int[D]));
+
+/* Création du tableau U qui contient l'évaluation de u_n en x_l */
+
+double *U = malloc(sizeof(int[D]));
+
+/* Initialisation de U  */
+
+for(l=0;l<D+1;l++){
+	U[l]=u0(xl[l]);
+}
 
 /* Le fichier doit renvoyer la fonction f3 app par la méthode d'Adomain sous le format double f3_Ad(x) */
 
