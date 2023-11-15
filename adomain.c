38101663a9e613a96fc02518b00e442512815798
Lucas Fourny <lucas28@Ubuntu22.myguest.virtualbox.org>
2023-11-15 15:48:42 +0100

    new files

--- adomain.c
+++ adomain.c
@@ -3,3 +3,19 @@
 #include <math.h>
 
 #include "fonctions.h"
+#include "methodesnum.h"
+#include "donnees.h"
+
+double alpha = 1.;
+
+double u0(double x){
+	double res;
+	res = (1./alpha)*h(x);
+	return res;
+}
+
+
+/* Le fichier doit renvoyer la fonction f3 app par la méthode d'Adomain sous le format double f3_Ad(x) */
+
+/* Créer une fonction pour intégrer la fonction calculée par la méthodes de Gauss */
+
