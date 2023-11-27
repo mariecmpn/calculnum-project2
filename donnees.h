f1240bc7ecc6f5c1a927324da45a378bbfe0446c
Marie COMPAIN <mariecompain@MacBook-Pro-de-Marie-2.local>
2023-11-27 17:14:19 +0100

    on corrige les betises

--- donnees.h
+++ donnees.h
@@ -5,6 +5,6 @@ double recup_L(float L);
 double recup_H(float H);
 int recup_M(float M);
 int recup_n(float n);
-int recup_nb_iter(float nb_iter);
+int recup_nb_iter(int nb_iter);
 
 #endif
--- noyaux.h
+++ noyaux.h
@@ -6,5 +6,6 @@ double k(double x, double t);
 double noyaux_iter(double x, double alpha);
 double inte_h(double x, int m, double alpha);
 double h(double x);
+double gauss_approx(double x, int n, double a, double b, double val[5]);
 
 #endif
